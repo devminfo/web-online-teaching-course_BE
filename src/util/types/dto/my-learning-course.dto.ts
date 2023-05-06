b3ae9e969184a07a744616f13eeefcce37fd6a66
minh chiu <himinhchiu@gmail.com>
2023-05-06 20:33:36 +0700

    feat: buycouse

--- src/util/types/dto/my-learning-course.dto.ts
+++ src/util/types/dto/my-learning-course.dto.ts
@@ -3,6 +3,10 @@ import { IsMongoId, IsNumber, IsOptional } from 'class-validator';
 export class MyLearningCourseDto {
   @IsOptional()
   @IsMongoId()
+  _id?: string;
+
+  @IsOptional()
+  @IsMongoId()
   idCourse: string;
 
   @IsOptional()
